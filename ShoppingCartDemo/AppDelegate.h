73e87cb74c87305bec84d257aa6b0ef40cb2ced0
Zhangping <594062584@qq.com>
2016-04-01 15:27:39 +0800

    Initial Commit

---
+++ ShoppingCartDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingCartDemo
+//
+//  Created by Zhangping on 16/4/1.
+//  Copyright © 2016年 zp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingCartDemoTests/ShoppingCartDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  ShoppingCartDemoTests.m
+//  ShoppingCartDemoTests
+//
+//  Created by Zhangping on 16/4/1.
+//  Copyright © 2016年 zp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShoppingCartDemoTests : XCTestCase
+
+@end
+
+@implementation ShoppingCartDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
