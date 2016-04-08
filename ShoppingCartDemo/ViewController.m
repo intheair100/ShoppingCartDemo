f47826e632d8268147577f55d5b4199f2875857a
Zhangping <594062584@qq.com>
2016-04-08 16:49:58 +0800

    add animation.

--- ShoppingCartDemo/ViewController.m
+++ ShoppingCartDemo/ViewController.m
@@ -43,19 +43,56 @@
     if (!layer) {
         btn.enabled = NO;
         layer =[CALayer layer];
-        layer.contents =  (__bridge id)[UIImage imageNamed:@"test01.jpg"].CGImage;
+        layer.contents =  (__bridge id)[UIImage imageNamed:@"thing.png"].CGImage;
         layer.contentsGravity = kCAGravityResizeAspectFill;
         layer.bounds = CGRectMake(0, 0, 50, 50);
+        layer.cornerRadius = CGRectGetHeight(layer.bounds)/2;
+        layer.masksToBounds= YES;
+        layer.position = CGPointMake(50, 150);
+        [self.view.layer addSublayer:layer];
+        
+ 
     }
+    [self addAnimation];
+}
+
+-(void)addAnimation
+{
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    animation.path = path.CGPath;
+    animation.rotationMode = kCAAnimationRotateAuto;
+    
+    
+    CABasicAnimation *expandAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    expandAnimation.duration = 0.5f;
+    expandAnimation.fromValue = [NSNumber numberWithFloat:1];
+    expandAnimation.toValue = [NSNumber numberWithFloat:2.0f];
+    expandAnimation.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    
+    CABasicAnimation *narrowAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    narrowAnimation.beginTime = 0.5;
+    narrowAnimation.fromValue = [NSNumber numberWithFloat:2.0f];
+    narrowAnimation.duration = 1.5f;
+    narrowAnimation.toValue = [NSNumber numberWithFloat:0.5f];
+    
+    narrowAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    
+    CAAnimationGroup *groups = [CAAnimationGroup animation];
+    groups.animations = @[animation,expandAnimation,narrowAnimation];
+    groups.duration = 2.0f;
+    groups.removedOnCompletion=NO;
+    groups.fillMode=kCAFillModeForwards;
+    groups.delegate = self;
+    [layer addAnimation:groups forKey:@"group"];
 }
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
